b6789523e221856685ffd5a44096e998972f9b00
Benton <chan.benton+test@gmail.com>
2017-02-14 00:15:29 +0000

    Created requestURL

--- download_avatars.js
+++ download_avatars.js
@@ -4,11 +4,11 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 // Pulls a list of contributors for a given git from repoOwner/repoName
 function getRepoContributors(repoOwner, repoName, cb) {
-	// var GITHUB_USER = "chan.benton+test@gmail.com";
-	// var GITHUB_TOKEN = "df8eaf3035c414619ad2cf9a7f2608c0e09f6f3e";
+	var GITHUB_USER = "chan.benton+test@gmail.com";
+	var GITHUB_TOKEN = "df8eaf3035c414619ad2cf9a7f2608c0e09f6f3e";
 
-	// var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + 
-	// 	'@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+	var requestURL = 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + 
+		'@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
 
 }
 
