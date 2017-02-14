d44cd34250cb4d94f0dbb58711a5a977df057624
Benton <chan.benton+test@gmail.com>
2017-02-14 02:38:18 +0000

    Refactored code as necessary

--- download_avatars.js
+++ download_avatars.js
@@ -1,25 +1,26 @@
 var request = require('request');
 var fs = require('fs');
 
+
+var owner = process.argv.slice(2,3)[0];
+var repository = process.argv.slice(3,4)[0];
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 var GITHUB_USER = "chan.benton+test@gmail.com";
 var GITHUB_TOKEN = "df8eaf3035c414619ad2cf9a7f2608c0e09f6f3e";
 
 // Pulls a list of contributors for a given git from repoOwner/repoName
-
 function downloadImageByURL(url, filePath) {
-  request.get(url)
-    .on('error', function (err) {                              
+  request(url)
+    .on('error', function (err) {                   
       throw err; 
     })
-    .on('response', function (response) {
-    })
     .pipe(fs.createWriteStream(filePath));
-    // concat avatar/login.jpg
 }
 function getRepoContributors(repoOwner, repoName, cb) {
   
+  // Custom HTTP Header in order to comply with GitHub's parameters to search.
   var options = {
   	url: 'https://' + GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
   	headers: {
@@ -27,14 +28,15 @@ function getRepoContributors(repoOwner, repoName, cb) {
   	}
   };
   
+  // GETs array and parses by JSON for legibility
   request(options, function (error, response, body){
   	var data = JSON.parse(body);
   	cb(data);
   });
-
 }
 
-getRepoContributors("jquery", "jquery", function(data){
+// Starts function to search repoContributors, callback provided to create dir "./avatars/" and download image
+getRepoContributors(owner, repository, function(data){
 	for (each in data){		
 		var profile = data[each];
 		dir = "./avatars/";
@@ -42,7 +44,7 @@ getRepoContributors("jquery", "jquery", function(data){
 		if (!fs.existsSync(dir)){
 			fs.mkdir(dir);
 		}
-		console.log(profile.login);
-		downloadImageByURL(profile, path)
+		downloadImageByURL(profile, path);
 	}
 });
+console.log("Program complete!");
